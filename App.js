3d0bd7da3df56f67ef46d85193131188cecc9000
Lucas Ribeiro <lucasribeiro2216@gmail.com>
2023-09-27 21:55:38 -0300

    update HomePage

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { BookBookmark, CalendarPlus, PawPrint } from 'phosphor-react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Div } from './assets/elements/common.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// #48ff9a
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
+    height: '100%',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f8f9fc',
+    fontSize: '2rem',
+    fontWeight: '600',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    padding: '2%',
   },
 });
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Div>
+          <Button>
+            <BookBookmark size={38} color="#48ff9a" />
+          </Button>
+          <Button>
+            <PawPrint size={38} color="#48ff9a" />
+          </Button>
+          <Button>
+            <CalendarPlus size={38} color="#48ff9a" />
+          </Button>
+        </Div>
+      </View>
+    );
+  }
+};
+
+export default App;
---
+++ assets/elements/common.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native'
+
+export const Div = styled.View`
+  width: 100%;
+  height: 10%;
+  background-color: rgba(20, 20, 21, 0.8);
+  border-radius: 20px;
+  justify-content: space-between;
+  flex-direction: row;
+  backdrop-filter: blur(10px);
+`
+export const Button = styled.TouchableOpacity`
+    width: 33%;
+    height: 100%;
+    border-radius: 20px;
+    background-color: transparent;
+    justify-content: center;
+    align-items: center;
+
+`;
