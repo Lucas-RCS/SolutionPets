674a5ee8d7bb246e935a2e28bc59d412672c530b
Lucas Ribeiro <lucasribeiro2216@gmail.com>
2023-10-02 16:46:54 -0300

    update Calendary

--- src/component/Calendary/Calendary.js
+++ src/component/Calendary/Calendary.js
@@ -1,10 +1,72 @@
-import React from 'react';
-import { View, KeyboardAvoidingView, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Modal, TouchableOpacity } from 'react-native';
+import DatePicker from 'react-native-modern-datepicker';
+import { getToday, getFormatedDate } from 'react-native-modern-datepicker';
 import style from './CalendaryCss.js';
+import { Div, DivCommon } from '../../elements/common.js';
+import { CalendarX, PlusCircle } from 'phosphor-react-native';
 
 export function Calendary() {
+
+  const today = new Date();
+
+  const startDate = getFormatedDate(today.setDate(today.getDate() + 1), 'YYYY/MM/DD');
+
+  const [open, setOpen] = useState(false);
+  const [date, setDate] = useState('');
+  // console.log(date);
+
+  function handleOnPress() {
+    setOpen(!open);
+  }
+  function handleChange(propDate) {
+    setDate(propDate);
+  }
+
   return (
     <View style={style.container}>
+      <TouchableOpacity onPress={handleOnPress}>
+        <DivCommon>
+          <Div style={style.openBtn}>
+            <PlusCircle size={32} weight='duotone' color='#141415' />
+            <Text style={style.textBtn}>
+              Adicionar Data
+            </Text>
+          </Div>
+        </DivCommon>
+      </TouchableOpacity>
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={open}
+      >
+        <View style={style.content}>
+          <View style={style.modalView}>
+            <DatePicker
+              mode='calendar'
+              selected={date}
+              minimumDate={startDate}
+              onDateChange={handleChange}
+              options={{
+                backgroundColor: '#141415',
+                textColor: '#f8f9fc', 
+                textHeaderColor: '#f8f9fc',
+                selectorColor: '#48ff9a', 
+                mainColor: '#48ff9a',
+                textDefaultColor: '#f8f9fc',
+              }}
+            />
+            <TouchableOpacity onPress={handleOnPress}>
+            <Div style={style.closeBtn}>
+            <CalendarX size={32} weight='duotone' color='#141415' />
+            <Text style={style.textBtn}>
+              Fechar
+            </Text>
+          </Div>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
--- src/component/Calendary/CalendaryCss.js
+++ src/component/Calendary/CalendaryCss.js
@@ -3,11 +3,60 @@ import { StyleSheet } from 'react-native';
 
 const style = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 0,
         width: '100%',
         height: '100%',
-        backgroundColor: '#141415',
+        backgroundColor: '#f8f9fc',
     },
- 
+    content: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#141415AA',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: '#141415',
+        borderRadius: 20,
+        width: '90%',
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    openBtn: {
+        width: '100%',
+        backgroundColor: '#48ff9a',
+        borderRadius: 12,
+        padding: 10,
+        alignItems: 'center',
+        textAlign: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    textBtn: {
+        color: '#141414',
+        fontSize: 18,
+        fontWeight: '700',
+    },
+    closeBtn: {
+        width: 300,
+        height: 60,
+        backgroundColor: '#48ff9a',
+        borderRadius: 12,
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+    },
+
 });
 export default style;
